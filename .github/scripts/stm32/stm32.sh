b64710fcf9ae479a3087b6a6820bf97be80e136c
antoniochavesgarcia <chaves@uma.es>
2025-03-21 08:42:53 +0100

    feat: Retrieving also .hex file

--- .github/scripts/stm32/stm32.sh
+++ .github/scripts/stm32/stm32.sh
@@ -10,5 +10,6 @@ source .github/scripts/init.sh
 build_stm32() {
     make ${MAKEOPTS} -C micropython/ports/stm32 BOARD=$1 USER_C_MODULES=../../../ulab all CFLAGS_EXTRA=-DULAB_HASH_STRING=$ulab_hash
     copy_files stm32/build-$1/firmware.dfu $1
+    copy_files stm32/build-$1/firmware.hex $1
     clean_up stm32 build-$1
 }
